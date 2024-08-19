4aff5d6f80f5915269a4e4b60370f746148a59c9
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-19 21:34:17 +0200

    feat: Sessions, Errors & Flash Messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel 10 Task List App</title>
+    @yield('styles')
 </head>
 
-<body>
+<body style="background-color: #595757">
     <h1>@yield('title')</h1>
     <div>
+        @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
+
         @yield('content')
     </div>
 

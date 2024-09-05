ead2bfbf496f3dd9c72a2324030c25a88c2c5c38
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-05 20:50:20 +0200

    feat: Toggle Task State

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,7 +4,8 @@
 @section('title', $task->title)
 
 @section('content')
-    <p>{{ $task->description }}</p>
+    <p>
+        {{ $task->description }}</p>
 
     @if ($task->long_description)
         <p>{{ $task->long_description }}</p>
@@ -15,9 +16,31 @@
     <p>Created at: {{ $task->created_at }}</p>
     <p>Updated at: {{ $task->updated_at }}</p>
 
-    <form method='POST' action="{{ route('tasks.destroy', ['task' => $task->id]) }}">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Delete</button>
-    </form>
+    <p>
+        @if ($task->completed)
+            <span style="color: green">Completed</span>
+        @else
+            <span style="color: red">Not completed</span>
+        @endif
+    </p>
+
+    <div>
+        <a href="{{ route('tasks.edit', ['task' => $task]) }}">Edit</a>
+    </div>
+
+    <div>
+        <form method='POST' action="{{ route('tasks.toggle-complete', ['task' => $task]) }}">
+            @csrf
+            @method('PUT')
+            <button type="submit">{{ $task->completed ? 'Mark as incomplete' : 'Mark as complete' }}</button>
+        </form>
+    </div>
+
+    <div>
+        <form method='POST' action="{{ route('tasks.destroy', ['task' => $task]) }}">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete</button>
+        </form>
+    </div>
 @endsection

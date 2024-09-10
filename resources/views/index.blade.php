1c6861b61320ed8ae6e27a62e926137ae98a32bb
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-10 21:01:20 +0200

    feat(UI): Style w/ TailwindCSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,19 +3,23 @@
 @section('title', 'The list of Tasks')
 
 @section('content')
-    <div>
-        <a href="{{ route('tasks.create') }}">Add Task</a>
-    </div>
+    <nav class="mb-4">
+        <a href="{{ route('tasks.create') }}"
+            class="font-medium bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-md hover:text-gray-900">Add
+            Task</a>
+    </nav>
     @forelse($tasks as $task)
         <div>
-            <a href="{{ route('tasks.show', ['task' => $task->id]) }}">{{ $task->title }}</a>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}" @class([
+                'line-through text-gray-500' => $task->completed,
+            ])>{{ $task->title }}</a>
         </div>
     @empty
         <li>No tasks found</li>
     @endforelse
 
     @if ($tasks->count())
-        <nav>
+        <nav class="mt-4">
             {{ $tasks->links() }}
         </nav>
     @endif

9d5783d5f97f3d10cc93e0f8ce31681b8e20155d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-23 20:49:46 +0200

    feat(UI/UX): Style Flash message with Alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel 10 Task List App</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
 
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
@@ -39,9 +40,21 @@
 
 <body class="container max-w-lg mx-auto mt-10 mb-10">
     <h1 class="mb-5 text-2xl">@yield('title')</h1>
-    <div>
+    <div x-data="{ flash: true }">
         @if (session()->has('success'))
-            <div>{{ session('success') }}</div>
+            <div x-show="flash"
+                class="relative mb-4 rounded border-green-500 bg-green-100 px-4 py-4 text-lg text-green-700 text-center"
+                role="alert">
+                <strong class="font-bold">Success!</strong>
+                <div>{{ session('success') }}</div>
+
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" @click="flash = false" class="w-6 h-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
+            </div>
         @endif
 
         @yield('content')
